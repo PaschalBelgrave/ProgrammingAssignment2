1a87e559096f92aff599df5aa8cd88cc697ce861
Paschal Belgrave <paschal.belgrave@gmail.com>
2014-05-24 12:14:35 -0400

    added coded to finx inverse and cache result

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	mat <- NULL
+	set <- function(y){
+		x <<- y
+		mat <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) mat <<-inverse
+	getinverse <- function() mat
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+	mat <- x$getinverse()
+
+	if(!is.null(mat)){
+		message("getting cached data")
+		return(mat)
+	}
+	data <- x$get()
+	mat <- solve(data, ...)
+	x$setmean(mat)
+	mat
 }
